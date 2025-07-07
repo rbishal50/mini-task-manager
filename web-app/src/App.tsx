c3b3aa3a614dcaec5c60df7ea38d85222de00c18
Bishal Rana <rbishal50@gmail.com>
2025-07-07 16:26:02 +0300

    [WebApp] Remove unnecessary stylings and add a basic dark mui theme

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,5 +1,20 @@
+import { Typography } from "@mui/material";
+
 function App() {
-  return <h1>Web App</h1>;
+  return (
+    <Typography
+      variant="h5"
+      sx={{
+        textAlign: "center",
+        fontWeight: 700,
+        color: "primary.main",
+        textTransform: "uppercase",
+        letterSpacing: "1px",
+      }}
+    >
+      Task Management Board
+    </Typography>
+  );
 }
 
 export default App;
